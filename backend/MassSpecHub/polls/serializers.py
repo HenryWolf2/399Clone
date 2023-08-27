6f4bd4ea59da996e4f7321e5c981a50b6ba352f6
HenryWolf2 <hen.wolf2003@gmail.com>
2023-08-27 21:18:20 +1200

    Adding post creation.
    Adding the post creation and serializer. Will need to update this to assign the author of the post.

--- backend/MassSpecHub/polls/serializers.py
+++ backend/MassSpecHub/polls/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,19 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'summary', 'description', 'publicity']
+
+    def create(self, validated_data):
+        post = Post(
+            title=validated_data['title'],
+            summary=validated_data['summary'],
+            description=validated_data['description'],
+            publicity=validated_data['publicity']
+        )
+        # author=validated_data['author'].id
+        post.save()
+        return post
