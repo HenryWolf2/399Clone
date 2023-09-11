3462fe8eda0d4fd4a76e3f1a21151a1ad06cfb85
LouisCanal <lcan880@aucklanduni.ac.nz>
2023-09-12 11:10:29 +1200

    added in redirects from the home and profile pages
    because home page has nothing on it yet - users are redirected to the profile page, if they do not have a token in the local storage then they are redirected to the login page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
+import { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/profile");;
+  },[])
   return (
     <div className="container">
       <NavigationBar />
--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 import AccountDetails from '../components/profile-details/AccountDetails';
+import { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Profile() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(localStorage.getItem('token') == null){
+      navigate("/login");
+    };
+  },)
   return (
     <div className="container">
       <NavigationBar />
