2f5ba973c42e1c3da6ae9640cf0b270adc2ce6ec
LouisCanal <lcan880@aucklanduni.ac.nz>
2023-09-12 13:53:05 +1200

    added the restrictedNavigationBar
    on login and register pages a restricted navigation bar is implemented

---
+++ frontend/src/components/restrictedNavigationBar.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import LogoTransparent from '../assets/images/LogoMSH_Transparent.png';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useEffect, useState } from 'react';
+import instance from './api/api_instance';
+
+
+function RestrictedAppBar() {
+  const logoStyle = {
+    paddingBottom: 10,
+    paddingTop: 10,
+  };
+
+  // adding in effect for getting the token from local storage
+  useEffect(() => {
+    if(localStorage.getItem('token') != null){
+      instance.defaults.headers.common['Authorization'] = 'Token ' + localStorage.getItem('token');
+      console.log(instance.defaults.headers.common['Authorization']);
+    }
+  },[])
+
+  return (
+    <AppBar position="static" sx={{backgroundColor:'black'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <div style={logoStyle}>
+            <img src={LogoTransparent} className="App-logo" alt="logo" width={50} />
+          </div>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            MSH
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default RestrictedAppBar;
