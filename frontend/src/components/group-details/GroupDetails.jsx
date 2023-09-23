4d5e8ce1c7d9407047cc9f78632d0d8eb09c94dc
Chafuyee <saunderszach88@gmail.com>
2023-09-24 04:00:43 +1300

    Update GroupDetails.jsx

--- frontend/src/components/group-details/GroupDetails.jsx
+++ frontend/src/components/group-details/GroupDetails.jsx
@@ -3,6 +3,7 @@ import Banner from '../../assets/images/group-template-banner.jpg';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import '../../assets/styles/global.css';
+import GroupBar from '../profile-details/GroupBar';
 
 function GroupDetails() {
   const [minimized, setMinimized] = useState(false);
@@ -15,6 +16,7 @@ function GroupDetails() {
     width: minimized ? '15%' : '50%',
     backgroundColor: 'grey', // Use 'backgroundColor' instead of 'backgroundColour'
     height: '1000px',
+    textAlign: minimized ? 'center' : 'left'
   };
 
   const bannerStyle = {
@@ -23,6 +25,8 @@ function GroupDetails() {
     flexDirection: 'column',
     backgroundImage: `url(${Banner})`,
     borderBottom: '3px solid white',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
     backgroundPosition: minimized ? 'center' : 'center left',
     boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.2)',
   };
@@ -46,6 +50,34 @@ function GroupDetails() {
     height: '60px',
   }
 
+  const overlayStyleExpanded = {
+    position: 'relative',
+    alignItems: 'center',
+    width: '500px',
+    marginRight: '40px',
+    height: '400px',
+    display: minimized ? 'none' : 'inline',
+  }
+
+  const overlayStyleMinimized = {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'right',
+    width: '56%',
+    marginRight: '40px',
+    height: 'auto',
+    display: minimized ? 'inline' : 'none',
+  }
+
+  const circleDot = {
+    width: '10px',
+    height: '10px',
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    marginTop: '35px',
+    marginRight: '15px',
+  }
+
   return (
     <div
       className={`minimizable-div ${minimized ? 'minimized' : ''}`}
@@ -57,11 +89,57 @@ function GroupDetails() {
         style={{
           height: minimized ? '750px' : '450px',
           display: 'flex',
-          flexDirection: 'column',
+          flexDirection: 'row',
           backgroundColor: '#35CFFF',
           boxShadow: '0px 8px 5px rgba(0, 0, 0, 0.2)',
+          justifyContent: 'right',
         }}
       >
+
+        <Avatar
+            sx={{
+              width: '225px',
+              height: '225px',
+              border: '4px solid white',
+              borderRadius: '50%',
+              position: 'relative',
+              marginTop: '40px',
+              marginRight: '40px',
+              float:'left',
+              display: minimized ? 'none' : 'flex',
+            }}
+          />
+        
+
+        <div style={overlayStyleExpanded}>
+          <h1 style={{color: 'white', marginTop: '50px'}}> University of Auckland </h1>
+          <div style={{display: 'flex', marginTop: '-40px', color: 'white', fontSize: '14px'}}>
+            <h1 style={{marginRight: '15px'}}>21.08.2023</h1>
+            <span style={circleDot}></span>
+            <h1>Public</h1>
+          </div>
+          <hr style={{width:'400px', border:' 2px solid #fff', marginRight: '450px',zIndex:900, marginTop: '-10px'}} />
+          <div style={{display: 'flex', marginTop: '-20px', color: 'white', fontSize: '14px'}}>
+            <h1 style={{marginRight: '15px'}}>7 Members</h1>
+            <span style={circleDot}></span>
+            <h1>11 Posts</h1>
+          </div>
+          <div style={{float:'left', marginTop: '-10px'}}><GroupBar></GroupBar></div>
+          <br></br>
+          <h1 style={{color: 'white', marginTop: '20px', fontSize: '27px'}}> Description </h1>
+          <hr style={{width:'400px', border:' 2px solid #fff', marginRight: '450px',zIndex:900, marginTop: '-10px'}} />
+          <p style={{fontSize: '15px', color: 'white'}}>The University of Auckland, founded in 1883, is New Zealand's oldest and largest university. Located in Auckland, it is renowned for its academic excellence, diverse programs, and strong international reputation. The university's commitment to research, innovation, and community engagement makes it a leading institution in the region and globally.</p>
+
+          
+
+        </div>
+
+        <div style={overlayStyleMinimized}> 
+            <h1 style={{color: 'white', marginTop: '100px', fontSize: '30px'}}> University of Auckland </h1>
+        </div>
+        
+
+
               {!minimized && (
         <Button className="zoom" style={minimizeButton} onClick={toggleMinimized}>
           &#10594;
@@ -72,6 +150,7 @@ function GroupDetails() {
           &#10596;
         </Button>
       )}
+
 
       </div>
 
