94706d3d4b74d116525bcb7721c68fca30928e7b
LouisCanal <lcan880@aucklanduni.ac.nz>
2023-09-28 20:03:23 +1300

    initial search bar implemented
    search bar implemented in public posts

--- frontend/src/pages/public-data.jsx
+++ frontend/src/pages/public-data.jsx
@@ -4,10 +4,35 @@ import IndividualPost from '../components/individual-posts/post';
 import instance from '../components/api/api_instance';
 import { useEffect } from 'react';
 import PostGrid from '../components/individual-posts/postgrid';
-import { Grid, Item, Box } from '@mui/material';
+import { Grid, Item, Box, Button } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment'
+import { Container, FormControlLabel, Stack } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default function Post() {
   const [PublicPosts, setPublicPosts] = useState([])
+  const [prompt, setPrompt] = useState("")
+  
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    console.log(prompt)
+    try{
+    await instance({
+        url: "post/search",
+        method: "GET",
+        params: {query: prompt}
+    }).then((res) => {
+        setPublicPosts(res.data);
+        
+    });
+    } catch(e){
+      console.error(e)
+    }}
 
   useEffect(() => {
     async function GetPublicPostsIDs() {
@@ -28,7 +53,7 @@ export default function Post() {
     [] // <-- empty dependency array
   ) 
 
-
+ 
 
   return (
     <div className="container">
@@ -37,6 +62,31 @@ export default function Post() {
 
         <h1> Public Posts </h1>
       </div>
+
+      <Container maxWidth= "sm">
+          <form onSubmit ={handleSubmit}>
+              <TextField
+              margin="normal"
+              fullWidth
+              value={prompt}
+              label="Search"
+              name="promt"
+              autoFocus
+              onChange={e => setPrompt(e.target.value)}
+              InputProps={{
+                  endAdornment: (
+                      <InputAdornment position="end">
+                      <SearchIcon />
+                      </InputAdornment>
+                  ),
+                  }}
+              />     
+          </form>
+          
+          
+          
+
+        </Container>
       
 
       <Box sx={{ flexGrow: 1, width: "98%", padding: "1%" }}>
