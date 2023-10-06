b38d8f1726788676b71fe0253ee642dde825cad9
Bowli3z <bpar545@aucklanduni.ac.nz>
2023-10-06 15:56:40 +1300

    Reverting some old changes

--- frontend/src/pages/group-specific.jsx
+++ frontend/src/pages/group-specific.jsx
@@ -6,16 +6,14 @@ import GroupData from '../components/group-details/GroupData';
 import { useEffect, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-export default function GroupSpecific() {
-
-  const {group_id} = useParams();
+export default function GroupSpecific(props) {
 
   return (
     <div className="container">
       <NavigationBar />
       <div style={{display: 'flex', flexDirection: 'row'}}>
-        <GroupDetails group_id={group_id} />
-        <GroupData group_id={group_id} />
+        <GroupDetails group_id={props.group_id} />
+        <GroupData group_id={props.group_id} />
       </div>
     </div>
   );
