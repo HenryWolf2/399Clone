0f1376f954a8d8058431e5e51cd833d0d71aacff
LouisCanal <lcan880@aucklanduni.ac.nz>
2023-10-06 15:34:16 +1300

    commit before a merge
    commit to save changes before merging in new sprint 6 changes

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,16 +3,31 @@ import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 import LoginForm from '../components/login/loginForm';
 import RestrictedNavigationBar from '../components/restrictedNavigationBar'
+import LoginLanding from '../components/login/loginLanding';
+
+import '../assets/styles/global.css';
 
 export default function Login() {
   return (
     <div className="container">
       <RestrictedNavigationBar/>
       <div className="App">
-        <div className='App-header'>
-          <div className='White-circle'>
-            <h2 style={{ color: '#000000', width: "350px" }}>Login to Mass Spectrometry Hub</h2>
-            <LoginForm/>
+        <div className='App-header' style={{backgroundAttachment: "fixed"}}>
+          <div className="circle-container">
+            <div className='White-circle'>
+              <h2 style={{ color: '#000000', width: "350px" }}>Login to Mass Spectrometry Hub</h2>
+              <LoginForm/>
+            </div>
+            <div style={{position: "absolute", bottom: "3vh",alignSelf:'center'}}>
+              <p className="landingText">Scroll for more</p>
+            </div>
+  
+          </div>
+          <div>
+            <div className='White-rectangle'>
+              <h2 style={{ color: 'black', width: "500px" }}>What is Mass Spectrometry Hub?</h2>
+              <LoginLanding/>
+            </div>
           </div>
         </div>
       </div>
