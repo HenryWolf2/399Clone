3500c4f8961ba32d0ab663ec91abee4cbc70d2ac
Henry Pettigrew <140290181+henrypnz@users.noreply.github.com>
2023-10-19 15:17:59 +1300

    New version of 404 page, not just a line of text

--- frontend/src/pages/notFound.jsx
+++ frontend/src/pages/notFound.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar';
 import MobileOverlay from '../components/MobileOverlay';
+import logo from '../assets/images/LogoMSH_Transparent.png';
+import '../assets/styles/global.css';
 
 export default function NotFound() {
   return (
     <div className="container">
       <MobileOverlay />
       <NavigationBar />
-      404 Page Not Found!
+      <div style={{ 
+      backgroundImage: `url(${logo})`,
+      backgroundRepeat: 'round',
+      backgroundPosition: 'top left',
+      backgroundSize: '30px',
+    }}
+    className = 'NotFound-background'>
+      <div className = 'NotFound-popup'>
+        <img src={logo} className="Post-logo" alt="logo" />
+        <h1 className = 'Overlay-title'>404: PAGE NOT FOUND</h1>
+        
+      </div>
+    </div>
     </div>
   );
 }
