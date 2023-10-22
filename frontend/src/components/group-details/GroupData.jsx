c2fb29ccfddd8f6986be15c183051784c18163f4
Chafuyee <saunderszach88@gmail.com>
2023-10-22 13:28:02 +1300

    fixed bug

--- frontend/src/components/group-details/GroupData.jsx
+++ frontend/src/components/group-details/GroupData.jsx
@@ -6,6 +6,7 @@ import GroupBar from '../profile-details/GroupBar';
 import PostGrid from '../individual-posts/postgrid';
 import Box from '@mui/material/Box';
 import instance from '../api/api_instance';
+import LogoTransparent from '../../assets/images/LogoMSH_Transparent.png';
 
 function GroupData(props) {
 
@@ -13,8 +14,32 @@ function GroupData(props) {
 
   const [groupname, setGroupname] = useState('')
   const [groupPosts, setGroupPosts] = useState([])
+  const [currentPerm, setCurrentPerm] = useState('')
+  const [canView, setCanView] = React.useState(false)
+  const [zeroPosts, setZeroPosts] = React.useState(false)
 
-  // Still need to organize user perms
+
+  useEffect(() => {
+    function checkZeroPosts() {
+      if (groupPosts.length !== 0) {
+        setZeroPosts(true);
+      } else {
+        setZeroPosts(false);
+      }
+    }
+    checkZeroPosts();
+  })
+
+  useEffect(() => {
+    function checkIsViewer() {
+      if (currentPerm == "owner" || currentPerm == "viewer" || currentPerm == "poster" || currentPerm == "admin") {
+        setCanView(true);
+      } else {
+        setCanView(false);
+      }
+    }
+    checkIsViewer();
+  })
 
   useEffect(() => {
     async function GetGroupInformation() {
@@ -25,6 +50,7 @@ function GroupData(props) {
           params: {group_id: props.group_id}    
       }).then((res) => {
         setGroupPosts(res.data.posts)
+        setCurrentPerm(res.data.user_permission)
         //Need to define perms
       });
       } catch(e) {
@@ -67,13 +93,27 @@ function GroupData(props) {
           flexDirection: 'row',
           backgroundColor: '#fff',
           boxShadow: '0px 8px 5px rgba(0, 0, 0, 0.2)',
-          justifyContent: 'right',
+          justifyContent: 'center',
           overflow: 'scroll',
         }}
       >
-        <Box sx={{ flexGrow: 1, width: "48.5%", padding: "5%" }}>
+        {canView ? (
+          <div>
+          {!zeroPosts ? (
+            <div style={{alignItems:'center', alignContent:'center', justifyContent:'center'}}>
+            <h1 style={{marginTop:'300px', height:'5%', backgroundColor:'#02AEEC', color:'white', padding:'20px', borderRadius:'10px', boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.2)',}}>New is No Group Data</h1>
+          </div>
+          ) : (
+            <Box sx={{ flexGrow: 1, width: "48.5%", padding: "5%" }}>
             <PostGrid narrow={true} post_array={groupPosts} />    
-        </Box>
+            </Box>
+          )}
+          </div>
+        ) : (
+          <div style={{alignItems:'center', alignContent:'center', justifyContent:'center'}}>
+            <h1 style={{marginTop:'300px', height:'5%', backgroundColor:'#02AEEC', color:'white', padding:'20px', borderRadius:'10px', boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.2)',}}>You Do Not Have Permission to View Group Data</h1>
+          </div>
+        )}
       </div>
 
 
